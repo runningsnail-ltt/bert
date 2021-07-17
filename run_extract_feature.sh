4b8e142a6bff321378ebd474dcbacb9fbae47081
tingting66 <tingting66@staff.weibo.com>
2021-07-17 16:54:18 +0800

    add

---
+++ run_extract_feature.sh
@@ -0,0 +1,13 @@
+export BERT_Chinese_DIR=/data2/tingting66/project/deep_ner/deep-baseline/coding/data/chinese_roberta_wwm_ext_L-12_H-768_A-12
+export Demo_DIR=data
+
+python extract_features.py \
+  --input_file="tmp.txt" \
+  --output_dir=./tmp/output.json \
+  --vocab_file=$BERT_Chinese_DIR/vocab.txt \
+  --bert_config_file=$BERT_Chinese_DIR/bert_config.json \
+  --init_checkpoint=$BERT_Chinese_DIR/bert_model.ckpt \
+  --max_seq_length=128 \
+  --batch_size=32 \
+  --layers=-1
+
